c3dc4df3c5ccba9faab3116b348b755fcd6dfcc0
kkikko <kkikkodev@naver.com>
2015-05-22 09:45:45 +0900

    dev version 0.4 : add processing menu

--- TetrisForDesktop/src/main.c
+++ TetrisForDesktop/src/main.c
@@ -7,62 +7,95 @@
 
 int main(int argc, char* argv[]){
 	TetrisView tetrisView;
-	TetrisView_StartGame(&tetrisView);
 	int processType;
 	int direction;
-	DWORD tickCount = GetTickCount();
-	while (True){
-		processType = AUTO;
-		direction = DOWN;
-		while (True){
-			if (GetTickCount() - tickCount > TetrisView_GetDownMilliSecond(&tetrisView)){
+	DWORD tickCount;
+	int isSetMainMenu = False;
+	do{
+		TetrisView_ProcessMainMenu(&tetrisView);
+		switch (tetrisView.mainMenu){
+		case START_MAIN_MENU:
+			TetrisView_StartGame(&tetrisView);
+			tickCount = GetTickCount();
+			while (True){
 				processType = AUTO;
 				direction = DOWN;
-				tickCount = GetTickCount();
-				break;
-			}
-			if (_kbhit()){
-				int key = _getch();
-				if (_kbhit()){
-					key = _getch();
-					if (key == UP_KEY_CODE){
-						processType = DIRECTION;
-						direction = UP;
-						break;
-					}
-					else if (key == DOWN_KEY_CODE){
-						processType = DIRECTION;
+				isSetMainMenu = False;
+				while (True){
+					if (GetTickCount() - tickCount > TetrisView_GetDownMilliSecond(&tetrisView)){
+						processType = AUTO;
 						direction = DOWN;
 						tickCount = GetTickCount();
 						break;
 					}
-					else if (key == LEFT_KEY_CODE){
-						processType = DIRECTION;
-						direction = LEFT;
-						break;
-					}
-					else if (key == RIGHT_KEY_CODE){
-						processType = DIRECTION;
-						direction = RIGHT;
-						break;
+					if (_kbhit()){
+						int key = _getch();
+						if (key == 0xE0 && _kbhit()){
+							key = _getch();
+							if (key == UP_KEY_CODE){
+								processType = DIRECTION;
+								direction = UP;
+								break;
+							}
+							else if (key == DOWN_KEY_CODE){
+								processType = DIRECTION;
+								direction = DOWN;
+								tickCount = GetTickCount();
+								break;
+							}
+							else if (key == LEFT_KEY_CODE){
+								processType = DIRECTION;
+								direction = LEFT;
+								break;
+							}
+							else if (key == RIGHT_KEY_CODE){
+								processType = DIRECTION;
+								direction = RIGHT;
+								break;
+							}
+						}
+						else{
+							if (key == SPACE_BAR_KEY_CODE){
+								processType = DIRECT_DOWN;
+								tickCount = GetTickCount();
+								break;
+							}
+							else if (key == ESC_KEY_CODE){
+								TetrisView_PauseGame(&tetrisView);
+								if (tetrisView.pauseMenu == MAIN_MENU_PAUSE_MENU){
+									isSetMainMenu = True;
+									break;
+								}
+								processType = AUTO;
+								tickCount = GetTickCount();
+								break;
+							}
+						}
 					}
 				}
-				else{
-					if (key == SPACE_BAR_KEY_CODE){
-						processType = DIRECT_DOWN;
-						tickCount = GetTickCount();
-						break;
-					}
-					else if (key == ESC_KEY_CODE){
-						TetrisView_PauseGame(&tetrisView);
-						processType = AUTO;
-						tickCount = GetTickCount();
-						break;
-					}
+
+				//for selecting main menu at pause menu
+				if (isSetMainMenu){
+					break;
+				}
+				TetrisView_ProcessGame(&tetrisView, processType, direction);
+				if (tetrisView.endMenu == RANKING_END_MENU){
+				
+				}
+				else if (tetrisView.endMenu == MAIN_MENU_END_MENU){
+					break;
+				}
+				else if (tetrisView.endMenu == EXIT_END_MENU){
+					tetrisView.mainMenu = EXIT_MAIN_MENU;
+					break;
 				}
 			}
+			break;
+		case RANKING_MAIN_MENU:
+			break;
+		case SETTING_MAIN_MENU:
+			break;
 		}
-		TetrisView_ProcessGame(&tetrisView, processType, direction);
-	}
+	} while (tetrisView.mainMenu != EXIT_MAIN_MENU);
 	return 0;
 }

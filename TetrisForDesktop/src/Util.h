bce8cfbbf275bb63e2e8f2ee1347da7528432b9b
kkikko <kkikkodev@naver.com>
2015-06-21 22:44:36 +0900

    dev version 0.6 : add cursor show, clear screen rectangle function

--- TetrisForDesktop/src/Util.h
+++ TetrisForDesktop/src/Util.h
@@ -5,14 +5,11 @@
 #define WINDOW_LINE_SIZE 25
 #define WINDOW_COL_SIZE 80
 
-enum FontColor{
-	BLACK, BLUE, GREEN, JADE, RED, PURPLE, YELLOW, WHITE, GRAY,
-	LIGHT_BLUE, LIGHT_GREEN, LIGHT_JADE, LIGHT_RED, LIGHT_PURPLE, LIGHT_YELLOW, LIGHT_WHITE, WHITE_INVERSION = 240
-};
-
 void CursorUtil_GotoXY(int x, int y);
+void CursorUtil_Show();
 void CursorUtil_Hide();
 void WindowUtil_ChangeWindowSize(int lines, int cols);
 void FontUtil_ChangeFontColor(int color);
+void ScreenUtil_ClearRectangle(int leftTopX, int leftTopY, int width, int height);
 
 #endif
